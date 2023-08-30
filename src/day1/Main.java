a64bcf5377896cbf32db8af488a4be6ddc421796
unknown <ekglchlrh@gmail.com>
2023-08-30 16:33:00 +0900

    자바기초실습

---
+++ src/day1/Main.java
@@ -0,0 +1,50 @@
+package day1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 출력
+        System.out.println(100);
+
+        // 문자 출력
+        System.out.println("100");
+
+        // 문자 출력
+        System.out.println("hello");
+
+        // 산술 연산
+        System.out.println(100+100);
+
+        // 문자 연산
+        System.out.println("100"+"100");
+
+        //문자, 숫자 처럼 값의 유형이 다르면 기능도 다르다.
+
+        // 자바에선 순자를 int라고 한다
+        //문자는 string라고 한다
+
+        //문자의 겨우 \n을 넣으면 줄이 바뀐다.
+        System.out.println("안녕하세요\n 반갑습니다.");
+
+        // 문자와 숫자를 더하기
+        System.out.println(100+"살"+"홍길동입니다.");
+
+        // 문자 포맷을 이용한 줄력
+        // %d는 숫자값이 들어간다.
+        System.out.printf("%d년생 %d살 홍길동입니다.", 2000, 22);
+
+        // 안녕하세요 저는 홍길동입니다.
+
+        System.out.println("안녕하세요 저는 홍길동입니다.");
+        System.out.println("안녕하세요 " + "저는" + " 홍길동입니다.");
+        System.out.print("안녕하세요 저는 홍길동입니다.");
+        System.out.print("안녕하세요 " + "저는" + " 홍길동입니다.");
+        System.out.println("안녕하세요\n 저는 홍길동입니다.");
+
+
+
+
+
+    }
+
+}
